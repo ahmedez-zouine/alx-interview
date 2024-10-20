7c1a75b00cae2b14b9850b5910ec997f30715684
azouine <azouine@gmail.com>
2024-10-20 19:35:32 +0000

    Done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,57 +1,35 @@
 #!/usr/bin/python3
-"""
-This module contains the minOperations function that calculates
-the fewest number of operations needed to result in exactly n H
-characters in the file.
-"""
+'''The minimum operations coding challenge.
+'''
 
 
-def next_operation(
-                   target: int,
-                   operations: int,
-                   current_characters: int,
-                   add_by: int
-                   ) -> int:
-    """
-    Recursive function to calculate the next operation
-    Args:
-        target (int): The target number of characters
-        operations (int): The number of operations
-        current_characters (int): The current number of characters
-        add_by (int): The number of characters to add by
-    Returns:
-        int: The number of operations
-    """
-    if target == current_characters:
-        return operations
-
-    if target % current_characters == 0:
-        return next_operation(
-                              target,
-                              operations + 2,
-                              current_characters * 2,
-                              current_characters
-                              )
-
-    return next_operation(
-                          target,
-                          operations + 1,
-                          current_characters + add_by,
-                          add_by
-                          )
-
-
-def minOperations(n: int) -> int:
-    """
-    Calculates the fewest number of operations needed to result in
-    exactly n H characters in the file.
-    Args:
-        n (int): The target number of characters
-    Returns:
-        int: The fewest number of operations needed to result in
-            exactly n H characters in the file.
-    """
-    if n <= 1:
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-
-    return next_operation(n, 2, 2, 1)
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
